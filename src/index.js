a32945645a3e2214fb9e345532dc135f0292c54e
Yusuf Bıyık <yusufbyk3660@gmail.com>
2020-07-02 19:33:27 +0300

    ➕ Add Group TODO Window
    A new window to add todo groups

--- src/index.js
+++ src/index.js
@@ -1,33 +1,80 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+
+if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 700,
+    webPreferences:{
+      nodeIntegration:true
+    }
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, './pages/index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  mainWindow.on("close", () => {
+    app.quit();
+  })
+
+  const appMenu = Menu.buildFromTemplate(appMenuTemplate);
+  Menu.setApplicationMenu(appMenu);
+
+  ipcMain.on("groupAdd", () => {
+    createGroupWindow()
+  })
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+const appMenuTemplate = [
+  {
+    label:"App",
+    submenu:[
+      {
+        label: "TODO Groups",
+        submenu:[
+          {
+            label: "Add"
+          },
+          {
+            label: "Edit"
+          },
+          {
+            label: "Delete"
+          }
+        ]
+      },
+      {
+        label:"Reload",
+        role:"reload"
+      },
+      {
+        label:"Close",
+        role:"close"
+      }
+    ]
+  },
+  {
+    label:"Development",
+    submenu: [
+      {
+        label:"DevTools",
+        role:"toggleDevTools"
+      }
+    ]
+  }
+]
+
+
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -35,12 +82,26 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+function createGroupWindow(){
+  addGroupWindow = new BrowserWindow({
+    width:500,
+    height:330,
+    resizable:false,
+    webPreferences:{
+      nodeIntegration:true
+    },
+    autoHideMenuBar:true,
+    title:"Add New TODO Group",
+  });
+
+  addGroupWindow.loadFile(path.join(__dirname, './pages/addGroup.html'));
+
+  addGroupWindow.on("close", () => {
+    addGroupWindow = null;
+  })
+}
