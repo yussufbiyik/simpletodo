dba0dcdc0d3ecd0c5c40b6525ff3fbef383659d7
Yusuf Bıyık <yusufbyk3660@gmail.com>
2020-07-14 03:54:48 +0300

    🔧 Stop devtools from opening

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, './pages/index.html'));
 
   // Open the DevTools.
-   mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   ipcMain.on("Go:HomePrintable",() => {
     mainWindow.loadFile(path.join(__dirname, './pages/printableTODOs.html'));
